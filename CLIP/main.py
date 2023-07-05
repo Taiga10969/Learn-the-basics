94c99cdee2a749695f58690719acb82f285b0ae0
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-07-05 16:52:31 +0900

    Update main.py

--- CLIP/main.py
+++ CLIP/main.py
@@ -5,7 +5,7 @@ from PIL import Image
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
-image = preprocess(Image.open("/taiga/experiment/CLIP/CLIP.png")).unsqueeze(0).to(device)
+image = preprocess(Image.open("/images/CLIP.png")).unsqueeze(0).to(device)
 text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
 
 with torch.no_grad():
