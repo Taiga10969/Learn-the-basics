d40c4d17e57679b75fb7e0c13f1d76f21bbb1c54
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-07-24 13:09:25 +0900

    Create basic.py

---
+++ Hugging_Face-transformers/BLIP-2/basic.py
@@ -0,0 +1,67 @@
+from transformers import AutoProcessor, Blip2ForConditionalGeneration
+import torch
+from PIL import Image
+
+# モデルの定義
+processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
+model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+
+
+image1 = Image.open("/taiga/experiment/BLIP-2/sample_img.jpg")       #愛犬の写真
+image2 = Image.open("/taiga/experiment/BLIP-2/sample_figure1.png")   #論文図1
+image3 = Image.open("/taiga/experiment/BLIP-2/sample_figure2.png")   #論文図2
+
+inputs = processor(image1, return_tensors="pt").to(device, torch.float16)
+generated_ids = model.generate(**inputs, max_new_tokens=20)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+print("img : ", generated_text)
+
+inputs = processor(image2, return_tensors="pt").to(device, torch.float16)
+generated_ids = model.generate(**inputs, max_new_tokens=20)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+print("figure1 : ", generated_text)
+
+inputs = processor(image3, return_tensors="pt").to(device, torch.float16)
+generated_ids = model.generate(**inputs, max_new_tokens=20)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+print("figure2 : ", generated_text)
+
+
+print('===== add prompt =====')
+
+prompt = "The types of this diagram are"
+print('prompt : ', prompt)
+
+inputs = processor(image2, text=prompt, return_tensors="pt").to(device, torch.float16)
+generated_ids = model.generate(**inputs, max_new_tokens=20)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+print("figure1 : ", generated_text)
+
+inputs = processor(image3, text=prompt, return_tensors="pt").to(device, torch.float16)
+generated_ids = model.generate(**inputs, max_new_tokens=20)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+print("figure2 : ", generated_text)
+
+
+## 学習に使えるような使用方法
+prompt = "Question: What type of diagram is this? Answer:"
+inputs = processor(images=image1, text=prompt, return_tensors="pt").to(device, torch.float16)
+print(inputs)
+
+inputs = {k: v.to(device) for k, v in inputs.items()}
+
+# ラベルのテンソル化
+label = "this diagram type is chart"
+label = processor(text=label, return_tensors="pt")["input_ids"]
+label = label.to(device)
+
+outputs = model(pixel_values=inputs["pixel_values"], input_ids=inputs["input_ids"], labels=label)
+
+print(outputs)
+#output = model(**inputs)
+attributes = dir(outputs)
+print(attributes)
+
