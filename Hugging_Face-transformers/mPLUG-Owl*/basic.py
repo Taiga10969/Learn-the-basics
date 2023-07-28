aaecb5a0c7b2812cdb4914731b01b0df4a457a5d
Taiga MASUDA <106218669+Taiga10969@users.noreply.github.com>
2023-07-28 19:40:39 +0900

    Create basic.py

---
+++ Hugging_Face-transformers/mPLUG-Owl*/basic.py
@@ -0,0 +1,34 @@
+# Load via Huggingface Style
+from transformers import AutoTokenizer
+import torch
+from mplug_owl.modeling_mplug_owl import MplugOwlForConditionalGeneration
+from mplug_owl.processing_mplug_owl import MplugOwlImageProcessor, MplugOwlProcessor
+
+pretrained_ckpt = 'MAGAer13/mplug-owl-llama-7b'
+model = MplugOwlForConditionalGeneration.from_pretrained(
+    pretrained_ckpt,
+    torch_dtype=torch.bfloat16,
+)
+image_processor = MplugOwlImageProcessor.from_pretrained(pretrained_ckpt)
+tokenizer = AutoTokenizer.from_pretrained(pretrained_ckpt)
+processor = MplugOwlProcessor(image_processor, tokenizer)
+
+# We use a human/AI template to organize the context as a multi-turn conversation.
+# <image> denotes an image placehold.
+prompts = [
+'''The following is a conversation between a curious human and AI assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.
+Human: <image>
+Human: Explain this image.
+AI: ''']
+
+# The image paths should be placed in the image_list and kept in the same order as in the prompts.
+# We support urls, local file paths and base64 string. You can custom the pre-process of images by modifying the mplug_owl.modeling_mplug_owl.ImageProcessor
+image_list = ['/taiga/experiment/mPLUG-Owl/IMG_1791.jpg']
+
+
+# generate kwargs (the same in transformers) can be passed in the do_generate()
+from pipeline.interface import do_generate
+sentence = do_generate(prompts, image_list, model, tokenizer, processor, 
+                       use_bf16=True, max_length=512, top_k=5, do_sample=True)
+
+print(sentence)
